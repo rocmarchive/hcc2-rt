d69e83ee6b2713e9df08b998c9827aefe59bb76f
Alexandre E. Eichenberger <alexe@us.ibm.com>
2017-07-20 09:56:56 -0400

    fix for the erronerous task test #10, was using an undefined scalar, and would work only on GPU using our current task implementation anyway; so disabled that sub-test

--- libomptarget/omptests/t-task/task.c
+++ libomptarget/omptests/t-task/task.c
@@ -12,6 +12,22 @@
 #define TRY_TASK 1
 #define TASK_COMPUTE 1
 
+
+#define OFF 1
+#define T1  1
+#define T1  1
+#define T2  1
+#define T3  1
+#define T4  1
+#define T5  1
+#define T6  1
+#define T7  1
+#define T8  1
+#define T9  1
+#define T10 1
+#define T11 1
+#define T12 1
+
 int main ()
 {
   int a[N], aa[N];
@@ -22,10 +38,13 @@ int main ()
   int i, errors;
   int cond = 0;
 
+#if OFF
   check_offloading();
+#endif
 
   // Test: task within target
 
+#if T1
   // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
@@ -60,9 +79,11 @@ int main ()
     if (b[i] != bb[i]) printf("%4i: got b %d, expected %d, error %d\n", i, b[i], bb[i], ++errors);
   }
   printf("#1 got %d errors\n", errors);
+#endif
 
   // Test: task within parallel
 
+#if T2
   // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
@@ -102,9 +123,11 @@ int main ()
     if (b[i] != bb[i]) printf("%4i: got b %d, expected %d, error %d\n", i, b[i], bb[i], ++errors);
   }
   printf("#2 got %d errors\n", errors);
+#endif
 
   // Test: multiple nested tasks in parallel region
 
+#if T3
     // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
@@ -154,9 +177,11 @@ int main ()
     if (b[i] != bb[i]) printf("%4i: got b %d, expected %d, error %d\n", i, b[i], bb[i], ++errors);
   }
   printf("#3 got %d errors\n", errors);
+#endif
 
   // Test: three successive tasks in a parallel region
 
+#if T4
   // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
@@ -203,9 +228,11 @@ int main ()
     if (b[i] != bb[i]) printf("%4i: got b %d, expected %d, error %d\n", i, b[i], bb[i], ++errors);
   }
   printf("#4 got %d errors\n", errors);
+#endif
 
   // Test: change of context when entering/exiting tasks
 
+#if T5
   // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
@@ -283,9 +310,11 @@ int main ()
   }
 
   printf("#5 got %d errors\n", errors);
+#endif
 
   // Test: change of context when using if clause
 
+#if T6
   // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
@@ -363,10 +392,12 @@ int main ()
   }
 
   printf("#6 got %d errors\n", errors);
+#endif
 
   // Test: final
 
-    // init
+#if T7
+  // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
     b[i] = bb[i] = 2*i +1;
@@ -415,10 +446,11 @@ int main ()
     if (b[i] != bb[i]) printf("%4i: got b %d, expected %d, error %d\n", i, b[i], bb[i], ++errors);
   }
   printf("#7 got %d errors\n", errors);
+#endif
 
-#if 0
   // Test: untied
 
+#if T8 && 0
   // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
@@ -472,7 +504,8 @@ int main ()
 
   // Test: mergeaeble
 
-    // init
+#if T9
+  // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
     b[i] = bb[i] = 2*i +1;
@@ -488,19 +521,19 @@ int main ()
       #pragma omp task firstprivate(id) shared(b)
       {
         PRINT("hi alex from  %d\n", id);
-        #pragma omp atomic
+        //#pragma omp atomic
         b[id]++;
 
         #pragma omp task firstprivate(id) shared(b) mergeable
         {
           PRINT("hi alex from  %d\n", id);
-          #pragma omp atomic
+          //#pragma omp atomic
           b[id]++;
 
           #pragma omp task firstprivate(id) shared(b) mergeable
           {
             PRINT("hi alex from  %d\n", id);
-            #pragma omp atomic
+            //#pragma omp atomic
             b[id]++;
           }
         }
@@ -521,8 +554,17 @@ int main ()
     if (b[i] != bb[i]) printf("%4i: got b %d, expected %d, error %d\n", i, b[i], bb[i], ++errors);
   }
   printf("#9 got %d errors\n", errors);
+#endif
 
   // Test: private
+
+#if T10 && 0
+  /*
+    Test disabled because this test only works on the GPU, where a
+    task is guaranteed to work on the same thread as it is
+    created. This is not true in general on the host. So we cannot use
+    this test generally. Thus I am disabling it here
+   */
 
   // init
   for(i=0; i<N; i++) {
@@ -540,6 +582,7 @@ int main ()
       #if TRY_TASK
         #pragma omp task private(id) shared(b)
         {
+          int id = omp_get_thread_num();
           #if TASK_COMPUTE
             PRINT("hi alex from  %d\n", id);
             b[id]++;
@@ -563,9 +606,11 @@ int main ()
     if (b[i] != bb[i]) printf("%4i: got b %d, expected %d, error %d\n", i, b[i], bb[i], ++errors);
   }
   printf("#10 got %d errors\n", errors);
+#endif
 
   // Test: depend
 
+#if T11
   // init
   int x;
   for(i=0; i<N; i++) {
@@ -610,9 +655,11 @@ int main ()
     if (b[i] != bb[i]) printf("%4i: got b %d, expected %d, error %d\n", i, b[i], bb[i], ++errors);
   }
   printf("#11 got %d errors\n", errors);
+#endif
 
   // Test: inverted priority
 
+#if T12
   // init
   for(i=0; i<N; i++) {
     a[i] = aa[i] = i+1;
@@ -659,7 +706,7 @@ int main ()
     if (b[i] != bb[i]) printf("%4i: got b %d, expected %d, error %d\n", i, b[i], bb[i], ++errors);
   }
   printf("#12 got %d errors\n", errors);
-
+#endif
 
   return 0;
 }
