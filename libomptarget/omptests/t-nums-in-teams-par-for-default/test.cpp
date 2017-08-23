e31cfc36b4035f10e596a35379e420adc56766de
Alexandre E. Eichenberger <eichen@tulgb005.cluster.com>
2017-08-23 13:24:56 -0400

    fix for change of default number of threads

--- libomptarget/omptests/t-nums-in-teams-par-for-default/test.cpp
+++ libomptarget/omptests/t-nums-in-teams-par-for-default/test.cpp
@@ -14,7 +14,10 @@ export LIBRARY_PATH="/home/eichen/eichen/lnew/obj/lib"
 
 #define DEBUG 0
 
-#define N 127*1024
+#define DEFAULT_NUM_TEAMS    128  /* from target_rtl.h lomp_DeviceRtl_CUDA*/
+#define DEFAULT_NUM_THREADS  1024 /* from target_rtl.h lomp_DeviceRtl_CUDA */ 
+#define N 127*1024                /* trip count, must be mutiple of DEFAULT_NUM_THREADS */
+
 int error=0;
 
 #define CHECK(_m, _a, _b) if ((_a) != (_b)) printf("  error for %s: got %d, expected %d, error %d\n", (_m), (_a), (_b), ++error)
@@ -29,6 +32,7 @@ int  main()
     printf("got num teams = %d\n", numFromEnv);
   }
 
+  ////////////////////////////////////////////////////////////////////////////////
   printf("test 1: use iteration trip count with default number of threads\n");
   int teams1, threads1;
   #pragma omp target teams distribute parallel for map(teams1, threads1) 
@@ -41,7 +45,6 @@ int  main()
     A[i] = 2*i;
   }  
 
-
   if (offloading_disabled()) {
     // Fake the output on the host
     CHECK("test1, teams", teams1, teams1);
@@ -50,15 +53,18 @@ int  main()
     if (numFromEnv) {
       CHECK("test1, teams", teams1, numFromEnv);
     } else {
-      CHECK("test1, teams", teams1, N/128);
+      CHECK("test1, teams", teams1, N / DEFAULT_NUM_THREADS);
     }
-    CHECK("test1, threads", threads1, 128);
+    CHECK("test1, threads", threads1, DEFAULT_NUM_THREADS);
   }
   printf("  completed\n");
 
+
+  ////////////////////////////////////////////////////////////////////////////////
   printf("test 2: use iteration trip count with 512 threads\n");
   int teams2, threads2;
-  #pragma omp target teams distribute parallel for map(teams2, threads2) thread_limit(512)
+  int threadLimit2 = 512;
+  #pragma omp target teams distribute parallel for map(teams2, threads2) thread_limit(threadLimit2)
   for(int i=0; i<N; i++) {
     if (i==0) {
       teams2 = omp_get_num_teams();
@@ -76,16 +82,19 @@ int  main()
     if (numFromEnv) {
       CHECK("test2, teams", teams2, numFromEnv);
     } else {
-      CHECK("test2, teams", teams2, N/512);
+      CHECK("test2, teams", teams2, N / threadLimit2);
     }
-    CHECK("test2, threads", threads2, 512);
+    CHECK("test2, threads", threads2, threadLimit2);
   }
-  
   printf("  completed\n");
 
+
+  ////////////////////////////////////////////////////////////////////////////////
   printf("test 3: use iteration trip count with 25 teams & 512 threads\n");
   int teams3, threads3;
-  #pragma omp target teams distribute parallel for map(teams3, threads3) num_teams(25) thread_limit(512)
+  int threadLimit3 = 512;
+  int teamLimit3 = 25;
+  #pragma omp target teams distribute parallel for map(teams3, threads3) num_teams(teamLimit3) thread_limit(threadLimit3)
   for(int i=0; i<N; i++) {
     if (i==0) {
       teams3 = omp_get_num_teams();
@@ -95,13 +104,12 @@ int  main()
     A[i] += 2*i;
   }  
   
-  CHECK("test3, teams", teams3, 25);
-  
+  CHECK("test3, teams", teams3, teamLimit3);
   if (offloading_disabled()) {
     // Fake the output on the host
     CHECK("test4, threads", threads3, threads3);
   } else {
-    CHECK("test4, threads", threads3, 512);
+    CHECK("test4, threads", threads3, threadLimit3);
   }
   printf("  completed\n");
 
