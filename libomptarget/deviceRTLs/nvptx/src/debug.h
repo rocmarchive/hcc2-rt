4256a1194637f08fda43cb2896e752ceade5aa82
Georgios Rokos <grokos@c460login01.c460cluster.net>
2017-11-08 11:38:38 -0500

    Fixed compilation bug.

--- libomptarget/deviceRTLs/nvptx/src/debug.h
+++ libomptarget/deviceRTLs/nvptx/src/debug.h
@@ -274,6 +274,7 @@
 #endif
 
 // print current state
+class omptarget_nvptx_TaskDescr;
 NOINLINE void PrintTaskDescr(omptarget_nvptx_TaskDescr *taskDescr, char *title,
                              int level);
 
